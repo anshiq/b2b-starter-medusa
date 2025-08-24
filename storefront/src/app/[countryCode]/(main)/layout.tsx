ea5ebef5849a4647b10a036c063f4dabc9a919b3
Anshik Singh <anshik.singh@silverpush.co>
2025-08-24 20:01:38 +0530

    turning my custom template

--- storefront/src/app/[countryCode]/(main)/layout.tsx
+++ storefront/src/app/[countryCode]/(main)/layout.tsx
@@ -8,6 +8,7 @@ import { NavigationHeader } from "@/modules/layout/templates/nav"
 import FreeShippingPriceNudge from "@/modules/shipping/components/free-shipping-price-nudge"
 import { StoreFreeShippingPrice } from "@/types/shipping-option/http"
 import { ArrowUpRightMini, ExclamationCircleSolid } from "@medusajs/icons"
+import { StoreCart } from "@medusajs/types"
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
@@ -26,24 +27,7 @@ export default async function PageLayout(props: { children: React.ReactNode }) {
   return (
     <>
       <NavigationHeader />
-      <div className="flex items-center text-neutral-50 justify-center small:p-4 p-2 text-center bg-neutral-900 small:gap-2 gap-1 text-sm">
-        <div className="flex flex-col small:flex-row small:gap-2 gap-1 items-center">
-          <span className="flex items-center gap-1">
-            <ExclamationCircleSolid className="inline" color="#A1A1AA" />
-            Build your own B2B store with this starter:
-          </span>
-
-          <a
-            className="group hover:text-ui-fg-interactive-hover text-ui-fg-interactive self-end small:self-auto"
-            href="https://git.new/b2b-starter-repo"
-            target="_blank"
-          >
-            GitHub Repo
-            <ArrowUpRightMini className="group-hover:text-ui-fg-interactive-hover inline text-ui-fg-interactive" />
-          </a>
-        </div>
-      </div>
-
+     
       {customer && cart && (
         <CartMismatchBanner customer={customer} cart={cart} />
       )}
@@ -55,7 +39,7 @@ export default async function PageLayout(props: { children: React.ReactNode }) {
       {cart && freeShippingPrices && (
         <FreeShippingPriceNudge
           variant="popup"
-          cart={cart}
+          cart={cart as StoreCart}
           freeShippingPrices={freeShippingPrices}
         />
       )}
